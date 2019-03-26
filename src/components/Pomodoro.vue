72d7582c6236491e40df975ce214959bff16f550
Rafael Pereira <rafael.pereira.sjbv@gmail.com>
2019-03-26 01:14:11 -0300

    Primeira versão do timer

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -1,13 +1,76 @@
 <template>
-    
+    <div class="box timer-pomodoro">
+          <h2 class="title" id="timer">
+            {{runTimer | prettify}}
+          </h2>
+    </div>
 </template>
 
 <script>
 export default {
+    name: 'Pomodoro',
+    data() {
+        return {
+            time: null,
+            timer: null,
+            pomodoro:25,
+            smallBreak:5,
+            longBreak:15
+        }
+    },
+    computed:{
+        runTimer: function(){
+            let time = this.time / 60
+            let minutes = parseInt(time)
+            let seconds = Math.round((time - minutes) * 60)
+            return minutes+":"+seconds
+        }
+    },
+    filters: {
+        prettify : function(value) {
+            let data = value.split(':')
+            let minutes = data[0]
+            let secondes = data[1]
+            if (minutes < 10) {
+                minutes = "0"+minutes
+            }
+            if (secondes < 10) {
+                secondes = "0"+secondes
+            }
+            return minutes+":"+secondes
+        }
+	},
+    methods:{
+        startPomodoro:function(){
+            this.time = (this.pomodoro * 60)
+        },
+        start () {
+            if (!this.timer) {
+                this.timer = setInterval( () => {
+                    if (this.time > 0) {
+                            this.time--
+                    } else {
+                            clearInterval(this.timer)
+                    }
+                }, 1000 )
+            }
+        }
+    },
+    beforeMount(){        
+        this.startPomodoro()
+        this.start()
+    }
 
 }
 </script>
 
-<style>
+<style scoped>
+.timer-pomodoro{
+    background-color: #c0392b;
+}
 
+#timer{
+    color: white;
+    text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="home">
-    
+  <div class="columns is-mobile is-centered">
+    <div class="column is-half">
+      <Pomodoro />
+    </div>
   </div>
 </template>
 
 <script>
+import Pomodoro from '../components/Pomodoro.vue'
 
 export default {
   name: 'home',
   components: {
-
+    Pomodoro
   }
 }
 </script>
+
+
